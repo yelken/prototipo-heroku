523517f97ea3b1d963dff17f2f856ba5be97007c
Yelken Gonzales <yelkezin@gmail.com>
2017-06-18 00:48:45 -0300

    vai carai

--- prototipo/wsgi.py
+++ prototipo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prototipo.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prototipo.settings.dev")
 
 application = get_wsgi_application()
