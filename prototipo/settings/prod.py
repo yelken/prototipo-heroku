ab3dfce23d49cda0b0e80fc004a7b30466b2fd12
Yelken Gonzales <yelkezin@gmail.com>
2017-06-18 01:27:48 -0300

    alter collect

--- prototipo/settings/prod.py
+++ prototipo/settings/prod.py
@@ -19,7 +19,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'prototipo',
-        'USER': 'root',
+        'USER': 'myuser',
         'PASSWORD': '',
         'HOST': ''
     }
