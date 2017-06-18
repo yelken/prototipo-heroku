e87c3a393a7bdefc6e22b838ff24a3d4ae2bde94
Yelken Gonzales <yelkezin@gmail.com>
2017-06-18 03:27:11 -0300

    Fix

--- prototipo/settings/base.py
+++ prototipo/settings/base.py
@@ -16,6 +16,7 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 def base_dir_join(*args):
     return os.path.join(BASE_DIR, *args)
